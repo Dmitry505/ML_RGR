f7baf68d3412649cf44c99aa29eb8d70e6b278b4
Dmitry505 <79017954+Dmitry505@users.noreply.github.com>
2024-03-12 18:56:37 +0600

    Update Main.py

--- scripts/Main.py
+++ scripts/Main.py
@@ -2,9 +2,9 @@ from st_pages import Page, add_page_title, show_pages
 
 show_pages(
     [
-        Page("Author.py"),
+
         Page("Information.py"),
-        Page("Visualization.py"),
-        Page("Prediction.py")
+        Page("Visualization.py")
+
     ]
 )
